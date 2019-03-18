863b98c1d7308327cf00a35e62642034841825d0
Sunat.P <31818562+SunatP@users.noreply.github.com>
2019-03-18 10:47:48 +0700

    improve logic of AID.cpp

--- Lab Teacher Worapan/Accessing Image Data.cpp
+++ Lab Teacher Worapan/Accessing Image Data.cpp
@@ -1,10 +1,13 @@
-#include<stdio.h>
+#include<stdio.h> // Standard io
 #include<opencv2/core/core.hpp>
 #include<opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
-#include <iostream>
-#include <cmath>
-#include<fstream>
+#include <iostream> // for output
+#include <cmath> // Calculate mathmatic
+#include<fstream> // Write file output
+#include <windows.h> // Add this header for waiting output before close automatically
+#include <iomanip> // add for decimal place
+#include <limits>
 
 using namespace std;
 using namespace cv;
@@ -21,13 +24,15 @@ int main()
 			int color = img.at<uchar>(cv::Point(x, y));
 			printf("Grey: %d\n", color);
 			total += (img.rows+img.cols);
-			fout << (char)color << " " << color << endl; // don't need to cast int to int
+			fout << "Gray Value is:  " << (int)color << endl; // don't need to cast integer value
 		}
 	}
-
 	 mean = (total / (img.rows * img.cols));
-	 fout << (char)mean << " " << mean << endl; // don't need to cast int to int
+	 fout << std::fixed << std::setprecision(2) << "The average of Gray value is " <<(double)mean << endl; // cast into double value
 	 fout.close();
-	printf("Average of Gray is : %f ", mean);
-	return 0; 
+	printf("Average of Gray is : %.2f \n", mean);
+	printf("Calculate Complete\n");
+	cout << "Press Enter to Continue";
+	cin.ignore();
+	return 0;
 }
